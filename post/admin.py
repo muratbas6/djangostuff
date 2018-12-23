7fb1ad2bddb2a9a69483ec2941448d6eeb2c68c7
muratbas6 <muratbas.dev@gmail.com>
2018-12-24 00:44:03 +0300

    admin panel configs

--- post/admin.py
+++ post/admin.py
@@ -3,5 +3,16 @@ from .models import yazi
 
 # Register your models here.
 
-admin.site.register(yazi)
 
+
+class adminYazi(admin.ModelAdmin):
+    list_display = ["başlık","yayinTarihi"]
+    list_display_links = ["yayinTarihi"]
+    list_filter = ["yayinTarihi"]
+    search_fields = ["başlık","metin"]
+    list_editable = ["başlık"]
+    class Meta:
+        model = yazi
+
+
+admin.site.register(yazi,adminYazi)
